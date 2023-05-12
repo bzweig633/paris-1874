8146e4a0fa8a04974ba8cfe5ee8b2b3dc2f25125
Benjamin Zweig <113474064+bzweig633@users.noreply.github.com>
2023-05-12 15:51:10 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-P0IXq7st9d8N5HfTU5oN47BepFQr40d2poxBbFyV0Q/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDBbsoa3SeZxjvNSBnt7HHA0GRv9Zm2yFU';
