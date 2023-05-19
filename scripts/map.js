3c6e420dc246c8da089e98db538ca77bc28955dc
Benjamin Zweig <113474064+bzweig633@users.noreply.github.com>
2023-05-19 18:27:46 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -141,6 +141,9 @@ $(window).on('load', function() {
 
     var group = L.featureGroup(markerArray);
     var clusters = (getSetting('_markercluster') === 'on') ? true : false;
+    clusters  = L.markerClusterGroup({
+                showCoverageOnHover: false
+    });
 
     // if layers.length === 0, add points to map instead of layer
     if (layers === undefined || layers.length === 0) {
