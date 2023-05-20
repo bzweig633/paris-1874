df059d39cc3c67e92c430f809013813fcdad2693
Benjamin Zweig <113474064+bzweig633@users.noreply.github.com>
2023-05-20 11:52:13 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -147,7 +147,13 @@ $(window).on('load', function() {
     if (layers === undefined || layers.length === 0) {
       map.addLayer(
         clusters
-        ? L.markerClusterGroup({polygonOptions: {color: "red"}}).addLayer(group).addTo(map)
+        ? L.markerClusterGroup({
+          polygonOptions: {
+            color: "yellow",
+            fill: "yellow,
+            weight: 1
+          }
+        }).addLayer(group).addTo(map)
         : group
       );
     } else {
