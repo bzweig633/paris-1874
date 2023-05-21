4554f994589deb78c4abe060b6b7a43a30b05ada
Benjamin Zweig <113474064+bzweig633@users.noreply.github.com>
2023-05-21 16:17:44 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -142,9 +142,6 @@ $(window).on('load', function() {
 
     
     var group = L.featureGroup(markerArray);
-    var clusters = L.markerClusterGroup({
-      polygonOptions: {color: "red"}
-    }); 
     var clusters = (getSetting('_markercluster') === 'on') ? true : false;
 
     // if layers.length === 0, add points to map instead of layer
