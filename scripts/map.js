6f3e436a6350292a1a34ca2f1e681a62d07778c9
Benjamin Zweig <113474064+bzweig633@users.noreply.github.com>
2023-05-23 14:38:51 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -139,21 +139,14 @@ $(window).on('load', function() {
       }
     }
 
-    
-    var group = L.featureGroup(markerArray); 
+    var group = L.featureGroup(markerArray);
     var clusters = (getSetting('_markercluster') === 'on') ? true : false;
 
     // if layers.length === 0, add points to map instead of layer
     if (layers === undefined || layers.length === 0) {
       map.addLayer(
         clusters
-        ? L.markerClusterGroup({
-          polygonOptions: {
-          color: 'red',
-          fillColor: 'red',
-          weight: 1
-          }
-        }).addLayer(group).addTo(map)
+        ? L.markerClusterGroup().addLayer(group).addTo(map)
         : group
       );
     } else {
